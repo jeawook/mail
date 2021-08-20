89e2589da6c7f5004c2e494a0a994fd1d2ff8f5b
jeawook <pjw4825@naver.com>
2021-08-20 15:58:35 +0900

    Create Mail.java

---
+++ src/main/java/com/system/mail/Mail.java
@@ -0,0 +1,27 @@
+package com.system.mail;
+
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Email;
+
+import static java.util.regex.Pattern.matches;
+
+@Embeddable
+@EqualsAndHashCode
+@Builder
+public class Mail {
+
+    @Email
+    private String address;
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "\""+name+"\"<"+address+">";
+    }
+}
