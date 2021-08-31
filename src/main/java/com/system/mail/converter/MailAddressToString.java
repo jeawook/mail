6f96cde2301ebd6023c56b8c5f0a4ab651d12c43
jeawook <pjw4825@naver.com>
2021-08-31 16:30:55 +0900

    Create MailAddressToString.java

---
+++ src/main/java/com/system/mail/converter/MailAddressToString.java
@@ -0,0 +1,14 @@
+package com.system.mail.converter;
+
+import com.system.mail.common.MailAddress;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class MailAddressToString implements Converter<MailAddress, String> {
+
+    @Override
+    public String convert(MailAddress source) {
+        return source.getName()+","+source.getAddress();
+    }
+}
