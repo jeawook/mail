9302dae4532a92117172e2e3c5ffc34dc90800f2
jeawook <pjw4825@naver.com>
2021-10-07 12:45:33 +0900

    Create MailProcessor.java

---
+++ src/main/java/com/system/mail/mailprocessor/MailProcessor.java
@@ -0,0 +1,14 @@
+package com.system.mail.mailprocessor;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MailProcessor {
+
+    private final MailHeaderEncoder mailHeaderEncoder;
+
+
+
+
+
+}
